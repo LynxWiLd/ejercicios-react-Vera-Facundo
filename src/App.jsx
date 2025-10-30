9fe5a1184b3d0b6ec178225fe06a7e0747d17794
Vera_Facundo <facundovera647@gmail.com>
2025-10-29 21:00:01 -0300

    agregos meta, la libreria bootstrap y el componente del primer ejercicio01

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Saludo from "./components/Saludo";
+
 function App() {
   return (
-    <h1>Hola Mundo</h1>
-  )
+    <>
+      <header className="py-3 text-center">
+        <h1 className="display-4">Primera App con React</h1>
+      </header>
+      <main>
+        <Saludo>  
+        </Saludo>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
